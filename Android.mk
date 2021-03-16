d5209f075b37ec512cd9e0c673caa449427feddc
David大伟 <davidmolinamorales1999@gmail.com>
2021-03-16 19:33:21 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -20,4 +20,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+ifeq ($(TARGET_DEVICE),tulip)
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
