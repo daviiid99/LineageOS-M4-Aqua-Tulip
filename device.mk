b52c31473aa2c9ddc9b60b6983074fc206d3a6b0
David大伟 <davidmolinamorales1999@gmail.com>
2021-03-16 20:42:48 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+LOCAL_PATH := device/sony/tulip-mainline
 # Device overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
