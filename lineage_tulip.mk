78e1769a20412e78018d91459bfa0702ee516dad
David大伟 <davidmolinamorales1999@gmail.com>
2021-03-16 20:55:45 +0100

    Update lineage_tulip.mk

--- lineage_tulip.mk
+++ lineage_tulip.mk
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # 
-
+LOCAL_PATH := device/sony/tulip-mainline
 # Inherit from those products. Most specific first
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -28,9 +28,3 @@ PRODUCT_DEVICE := tulip-mainline
 PRODUCT_MODEL := Xperia M4 Aqua
 PRODUCT_BRAND := sony
 PRODUCT_MANUFACTURER := Sony
-
-TARGET_VENDOR_PRODUCT_NAME := tulip-mainline
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V10.3.6.0.PFGMIXM release-keys"
-
-BUILD_FINGERPRINT := xiaomi/lavender/lavender:9/PKQ1.180904.001/V10.3.6.0.PFGMIXM:user/release-keys
